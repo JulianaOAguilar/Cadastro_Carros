43d22fb67b09e5e0847143e67eef33d6f93867f4
JulianaOAguilar <julianaoaguilar16@gmail.com>
2025-10-07 14:54:59 -0300

    :sparkles: Closes #2 feat: Componente header

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Header } from './components/header/header';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header
   ],
   imports: [
     BrowserModule,
