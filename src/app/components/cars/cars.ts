03b200a180938cae50ac369f1874740ae50ccb16
JulianaOAguilar <julianaoaguilar16@gmail.com>
2025-10-08 08:38:00 -0300

    :sparkles: Closes #9: Botões adicionar e remover

--- src/app/components/cars/cars.ts
+++ src/app/components/cars/cars.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Car} from '../../car.model'
+import { Car } from '../../car.model'
 
 
 @Component({
@@ -9,42 +9,45 @@ import { Car} from '../../car.model'
   styleUrl: './cars.css'
 })
 export class Cars {
-isUpdate: boolean = false;
-car: Car = {} as Car;
-cars: Car[] = [
-  {
-    id: 1,
-    name: 'Fusca',
-    assembler: 'Tadeu',
-    price: 5000.50,
-    year: 1860
-  },
+  isUpdate: boolean = false;
+  car: Car = {} as Car;
+  cars: Car[] = [
+    {
+      id: 1,
+      name: 'Fusca',
+      assembler: 'Tadeu',
+      price: 5000.50,
+      year: 1860
+    },
 
-  {
-    id: 2,
-    name: 'Renault Sandero',
-    assembler: 'Juju Clara Car',
-    price: 100000.50,
-    year: 2010
-  }
-]
+    {
+      id: 2,
+      name: 'Renault Sandero',
+      assembler: 'Juju Clara Car',
+      price: 100000.50,
+      year: 2010
+    }
+  ]
 
   saveCar() {
 
-    if(!this.isUpdate)
-    {
+    if (!this.isUpdate) {
       this.car.id = this.cars.length + 1;
       this.cars.push(this.car);
     }
-    
+
     this.car = {} as Car;
     this.isUpdate = false;
   }
 
-  
-update(selectedCar: Car) {
-this.car = selectedCar;
-this.isUpdate = true;
-}
+
+  update(selectedCar: Car) {
+    this.car = selectedCar;
+    this.isUpdate = true;
+  }
+
+  remove(selectedCar: Car) {
+    this.cars = this.cars.filter(b => b !== selectedCar);
+  }
 }
 
